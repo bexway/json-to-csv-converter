29f2dc7be9aad51168657dd7e8f8b3e873e4ab3f
Rebecca Way <rebeccaway2016@u.northwestern.edu>
2016-08-01 18:44:36 -0500

    Final touches
    Made the code more generalizable (it takes arguments now!), added
    another practice file, and updated the README

--- README.md
+++ README.md
@@ -2,10 +2,7 @@
 A python script that will turn a bunch of json files into csv files, and one aggregate csv.
 
 # How to use
-It isn't quite done yet, but when it is, this is how it will work:
 
-1. Download the files into a folder.
-2. (optional) Put all your json files into one folder named "json"
-3. (optional) Put the "json" folder in the same directory as the python script
-4. Run the python script (in terminal, use cd to navigate to the script directory, then use the command "python json-to-csv.py".)
-If you didn't perform the optional steps above, give the program two arguments: the first is the directory containing your json files. The second is the folder where you want them to be saved.
+1. Download the files from Github into a folder.
+2. Run the python script (in terminal, use cd to navigate to the script directory, then use the command "python json-to-csv.py".)
+3. You will be prompted for paths to the directory containing your json files, and the directory to save the resulting csv files in. Enter valid (existing) directories. (Leave the input empty to use the current directory. The program should exclude any files that don't end in '.json')
--- json-to-csv.py
+++ json-to-csv.py
@@ -8,64 +8,81 @@ import sys
 
 
 def json_to_csv(directory="json/", destination=""):
-    #directory = "json/"
+
+    response = raw_input("Would you like to create an aggregate file containing all data?"+
+        " This will only work if all of your data files have the same headers."+
+        " (Enter 'y' for yes, and anything else for no.):")
+    if response == "y":
+        agg = True
+    else:
+        agg = False
+
+    directory = raw_input("Please enter the path for the directory containing your json files."+
+        " (Leave the input empty to look in the current directory.):")
+    destination = raw_input("Please enter the path for the directory where the resulting csv files should be saved."+
+        " (Leave the input empty to look in the current directory.):")
+
     #double-check that the directory ends in a '/' for filereading purposes
-    if directory[-1] != "/":
-        directory += "/"
+    if directory:
+        if directory[-1] != "/":
+            directory += "/"
+    if destination:
+        if destination[-1] != "/":
+            destination += "/"
 
     #read in all the files from the directory
     files = [f for f in listdir(directory) if isfile(join(directory, f))]
     filepaths = []
-    data = []
+    data_files = []
     for f in files:
         if f[-5:] == ".json":
-            filepaths += [directory + f]
+            filepaths += [f]
         else:
             print "File " + f + " isn't a json file! It won't be included."
 
     #add all the data and filenames (for saving purposes) to one list
     for f in filepaths:
-        print f
-        jsonfile = open(f)
-        data += [json.load(jsonfile), f]
+        jsonfile = open(directory + f)
+        data_files.append({"data":json.load(jsonfile), "filename":f[:-5]})
 
-    #For each file
-    for d in data:
-        d_keys = d.keys()
-        headers = []
-        for x in xrange(0,len(d[d_keys[0]])):
-            headers += [d[d_keys[0]][x].keys()]
-
-        headers = [item for sublist in headers for item in sublist]
-        headers = list(set(headers))
-
-        csvfile = open('csv/aggregate_data.csv', 'wb')
-        aggregatewriter = csv.DictWriter(csvfile, headers)
+    #Open a file for aggregate data, using headers from the first file
+    if agg:
+        agg_headers = data_files[0]["data"][0].keys()
+        csvfile = open(destination + 'aggregate_data.csv', 'wb')
+        aggregatewriter = csv.DictWriter(csvfile, agg_headers)
         aggregatewriter.writeheader()
 
-        for data_key in d_keys:
-            #for each subject
-            curr_data = d[data_key]
-            subjectfile = open("csv/" + str(data_key) + '.csv', 'wb')
-            subjectwriter = csv.DictWriter(subjectfile, headers)
-            subjectwriter.writeheader()
-            for trial in curr_data:
-                rowDict = {}
-                for trial_key in trial.keys():
-                    rowDict[trial_key] = trial[trial_key]
-                subjectwriter.writerow(rowDict)
-                aggregatewriter.writerow(rowDict)
-            subjectfile.close()
+    #For each file
+    for d in data_files:
+        data = d["data"]
+        filename = d["filename"]
+        headers = data[0].keys()
+
+        subjectfile = open(destination + filename + '.csv', 'wb')
+        subjectwriter = csv.DictWriter(subjectfile, headers)
+        subjectwriter.writeheader()
+
+        #for each row in the file's data
+        for row in data:
+
+            # for trial in curr_data:
+            #     rowDict = {}
+            #     for trial_key in trial.keys():
+            #         rowDict[trial_key] = trial[trial_key]
+            subjectwriter.writerow(row)
+            if agg:
+                aggregatewriter.writerow(row)
+        #close the individual file before moving on
+        subjectfile.close()
 
 
 
-
+    if agg:
         csvfile.close()
-        return
+    return
 
 
 
 #Makes it so the function can be run from the terminal, but can be imported to other files
 if __name__ == "__main__":
-    print 'Number of arguments:', len(sys.argv), 'arguments.'
     json_to_csv()
